187aa7d375a582e1357ffdd31639c3df59222432
dpesce <dom.pesce@gmail.com>
2022-07-25 11:52:58 -0400

    rebuilt docs

--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.55.gd9463ec',
+    VERSION: '0+untagged.55.gd9463ec.dirty',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
