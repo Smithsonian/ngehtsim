01c36acf6928f556d2f21da0d64b1710bb516029
dpesce <dom.pesce@gmail.com>
2022-07-30 21:25:14 -0400

    rebuilt docs

---
+++ html/_sources/docs/source/weather.rst.txt
@@ -0,0 +1,9 @@
+.. module:: weather
+
+Weather utilities
+=========================
+
+Definition of the weather interface.
+
+.. automodule:: weather.weather
+   :members:
--- html/_static/documentation_options.js
+++ html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.68.g0242acd',
+    VERSION: '0+untagged.72.gf4ae77e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
