fc81f36d02136b0aa41d93c9962beae5435f57ba
dpesce <dom.pesce@gmail.com>
2023-03-30 20:17:45 -0400

    update space example

--- examples/example_data_generation_space/generate_observation.py
+++ examples/example_data_generation_space/generate_observation.py
@@ -9,16 +9,16 @@ import ngehtsim.obs.obs_plotter as op
 #######################################################
 # generate an observation
 
-sites = ['ALMA','APEX','GLT','JCMT','KP','LMT','NOEMA',
-         'PV','SMA','SMT','SPT','space']
+sites = ['ALMA','APEX','GLT','JCMT','KP','LMT','NOEMA','PV','SMA','SMT','SPT','space']
 settings = {'sites': sites,
-            'D_new': 100.0}
+            'D_new': 3.5,
+            'frequency': 230}
 
 # input settings file
 yamlfile = './settings.yaml'
 
 # initialize the observation generator
-obsgen = og.obs_generator(settings,settings_file=yamlfile)
+obsgen = og.obs_generator(settings,settings_file=yamlfile,ephem='ephemeris/space')
 
 # generate the observation
 obs = obsgen.make_obs()
@@ -30,3 +30,6 @@ obs.save_uvfits('./example_datafile.uvfits')
 # make some plots of the data
 
 op.plot_uv(obs,filename='./example_plot_uv.png',umax=60)
+op.plot_amp(obs,filename='./example_plot_amp.png')
+op.plot_phase(obs,filename='./example_plot_phase.png')
+op.plot_snr(obs,filename='./example_plot_snr.png')
