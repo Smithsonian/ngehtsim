1a5c288fa4adcfcfaeee842f1b0d438d0b35da92
dpesce <dom.pesce@gmail.com>
2026-07-08 20:56:01 -0400

    add automated documentation deployment

--- conf.py
+++ conf.py
@@ -49,12 +49,12 @@ source_suffix = '.rst'
 master_doc = 'docs/source/index'
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['docs/source/_templates']
+templates_path = []
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['docs/_build']
 
 # -- Options for HTML output -------------------------------------------------
 
@@ -66,7 +66,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['docs/source/_static']
+html_static_path = []
 
 # Example configuration for intersphinx: refer to the Python standard library.
 # intersphinx_mapping = {'https://docs.python.org/': None}
