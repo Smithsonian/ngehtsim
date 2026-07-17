bc0582d814c32b0aca4ee98696246e6137f8265d
dpesce <dom.pesce@gmail.com>
2026-07-17 11:41:45 -0400

    Add internal visibility dataset foundation

--- ngehtsim/obs/__init__.py
+++ ngehtsim/obs/__init__.py
@@ -4,5 +4,5 @@ Tools for generating and plotting observations.
 
 __author__ = "Dom Pesce"
 
-__all__ = ['obs_generator','obs_plotter']
+__all__ = ['obs_generator', 'obs_plotter', 'visibility_dataset']
 from . import *
